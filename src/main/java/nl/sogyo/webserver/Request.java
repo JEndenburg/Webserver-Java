feb2b1c58f2599bad720b49bf2c843409e227b2a
Jeroen Endenburg <jendenburg@sogyo.nl>
2019-10-14 15:02:05 +0200

    Restructured request

--- src/main/java/nl/sogyo/webserver/Request.java
+++ src/main/java/nl/sogyo/webserver/Request.java
@@ -1,34 +1,65 @@
 package nl.sogyo.webserver;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.Map;
+import java.util.Set;
 
 /// Representation of the incoming HTTP request.
-public interface Request {
-
+public class Request 
+{
+	private final HttpMethod method;
+	private final String resourcePath;
+	private final Map<String, String> headerParameters;
+	private final Map<String, String> urlParameters;
+	
+	private Request(HttpMethod method, String resourcePath, Map<String, String> headerParameters, Map<String, String> urlParameters)
+	{
+		this.method = method;
+		this.resourcePath = resourcePath;
+		this.headerParameters = headerParameters;
+		this.urlParameters = urlParameters;
+	}
+	
     /// Defines the HTTP method that was used by the
     /// client in the incoming request.
-    HttpMethod getHTTPMethod();
+    public HttpMethod getHTTPMethod()
+    {
+    	return method;
+    }
 
     /// Defines the resource path that was requested by
     /// the client. The resource path excludes url parameters.
-    String getResourcePath();
+    public String getResourcePath()
+    {
+    	return resourcePath;
+    }
 
     /// Defines the names of the header parameters that
     /// were supplied in the request.
-    List<String> getHeaderParameterNames();
+    public Set<String> getHeaderParameterNames()
+    {
+    	return headerParameters.keySet();
+    }
 
     /// Retrieves the supplied header parameter value
     /// correspronding to the name. If no header exists
     /// with the name, it returns null.
-    String getHeaderParameterValue(String name);
+    public String getHeaderParameterValue(String name)
+    {
+    	return headerParameters.get(name);
+    }
 
     /// Retrieves the URL parameters that were present in
     /// the requested URL.
-    List<String> getParameterNames();
+    public Set<String> getParameterNames()
+    {
+    	return urlParameters.keySet();
+    }
 
     /// Retreives the URL parameter value corresponding to
     /// the name. If no parameter exists with the name,
     /// it returns null.
-    String getParameterValue(String name);
+    public String getParameterValue(String name)
+    {
+    	return urlParameters.get(name);
+    }
 }
